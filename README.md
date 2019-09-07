9f4597e44136514a284b835096a0c373cb6cd226
Jihakuz <Jihakuz@gmail.com>
2019-09-07 11:46:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Batch Audio Track Remover
+## Tom Flux
+
+### Intro
+This is a simple program that will keep the video and audio track from a video file on batch.
+
+This is useful for example when an anime has audio track for both Japanese and English, however you only want to keep the English track and cut down on file sizes. Instead of running every file through a program like handbrake, this program will do it all automatically.
+
+### Installation
+**FFMPEG IS REQUIRED** for this to work, along with Python 3+.
---
+++ main.py
@@ -0,0 +1,26 @@
+from os import system, listdir, path
+
+input_path = rf"{input('Please enter the input path: ')}"
+output_path = rf"{input('Please enter the output path: ')}"
+
+command = 'ffmpeg -i {input_file}'
+
+if input("Scale video? Y/n   ").lower() != 'n':
+    command += f' -vf scale={input("Please enter the width: ")}x{input("Please enter the height: ")} -max_muxing_queue_size 9999'
+
+
+if input("Remove audio tracks? Y/n   ").lower() != 'n':
+    command += f' -map 0:0 -map 0:{input("Please enter the audio track: ")} -acodec copy'
+
+command += ' {output_file}'
+
+for file in listdir(input_path):
+    settings = {
+        'input_file': f'"{path.join(input_path, file)}"',
+        'output_file': f'"{path.join(output_path, file)}"'
+    }
+
+    current_command = command.format(**settings)
+
+    print(current_command)
+
